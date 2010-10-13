bfd4f468b49c1101e8141795acd6b6d1d888a6d6
func09 <mitsuru.haga@gmail.com>
2010-10-14 00:10:21 +0900

    あ

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Borndays
     config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
