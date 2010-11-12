759b3c0248c5f20a2a73d62f74054d2a0059fcfa
Teppei MACHIDA <machida@fjord.jp>
2010-11-12 09:30:53 +0900

    add birthday message

---
+++ public/javascripts/message.js
@@ -0,0 +1,18 @@
+jQuery.event.add(window, "load", function(){
+      $(".message").animate(
+{
+"top": "+=230px",
+"opacity":"1",
+}, "slow");
+}
+);
+
+jQuery.event.add(window, "load", function(){
+      $(".message").animate(
+{
+"opacity":"1",
+}, "slow");
+}
+);
+
+
